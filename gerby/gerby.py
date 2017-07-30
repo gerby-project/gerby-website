5c13cfeacd23fd8ccd1c3a1a6a31c51adca20847
Pieter Belmans <pieterbelmans@gmail.com>
2017-07-30 19:48:10 -0400

    Building the search query and output

--- gerby/gerby.py
+++ gerby/gerby.py
@@ -4,10 +4,11 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 import time
 
 from peewee import *
+from playhouse.sqlite_ext import *
 
-FILENAME = "stacks.sqlite"
+FILENAME = "htt.sqlite"
 
-db = SqliteDatabase(FILENAME)
+db = SqliteExtDatabase(FILENAME)
 
 # TODO this should be taken from a different package?
 # TODO or should the tools and the server be in the same package?
@@ -26,6 +27,14 @@ class Tag(BaseModel):
   # allows us to sort tags according to their reference
   def __gt__(self, other):
     return tuple(map(int, self.ref.split("."))) > tuple(map(int, other.ref.split(".")))
+
+class TagSearch(FTSModel):
+  tag = SearchField()
+  html = SearchField() # HTML of the statement or (sub)section
+  full = SearchField() # HTML of the statement including the proof (if relevant)
+
+  class Meta:
+    database = db
 
 class Proof(BaseModel):
   tag = ForeignKeyField(Tag, related_name = "proofs")
@@ -76,6 +85,10 @@ def show_tag(tag):
     return render_template("show_chapter.html", chapter=tag, sections=sections)
 
   else:
+    # TODO maybe always generate the breadcrumb data, but only pass it if at least 3 levels deep?
+    # we could have a top breadcrumb if 3 levels deep
+    # and an "overview where you're at", on the right now, as many levels as necessary?
+
     # if something is at least 3 levels deep we show a breadcrumb
     breadcrumb = None
     if len(tag.ref.split(".")) > 2:
@@ -85,6 +98,12 @@ def show_tag(tag):
 
       # TODO can we do a select with join without specifying all the columns?
       breadcrumb = sorted(Tag.select(Tag.tag, Tag.ref, Tag.type, LabelName.name).join(LabelName).where(Tag.ref << parents))
+
+    # if something is a section, we allow people to navigate by section
+    sections = None
+    if tag.type == "section":
+      # TODO just put in an extra column in Tag, with the in-text order of things, to make life easier...
+      pass
 
     proofs = Proof.select().where(Proof.tag == tag.tag)
 
@@ -96,3 +115,12 @@ def show_chapters():
   chapters = sorted(chapters)
 
   return render_template("show_chapters.html", chapters=chapters)
+
+@app.route("/search")
+def show_search():
+  # TODO not sure whether this is an efficient query: only fulltext and docid is quick apparently
+  # TODO can we use TagSearch.docid and Tag.rowid or something?
+  # TODO can we match on a single column? maybe we need two tables?
+  query = "Eilenberg"
+  results = Tag.select(Tag, TagSearch, TagSearch.rank().alias("score")).join(TagSearch, on=(Tag.tag == TagSearch.tag).alias("search")).where(TagSearch.match(query), Tag.type.not_in(["chapter", "section", "subsection"]))
+  return render_template("show_search.html", results=results)
