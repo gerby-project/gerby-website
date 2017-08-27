a0a6ce4b6fc2bdb8fa0fe797456be82ebf7e2b9a
Pieter Belmans <pieterbelmans@gmail.com>
2017-08-27 12:03:20 +0200

    Implemented working search function

--- gerby/gerby.py
+++ gerby/gerby.py
@@ -92,15 +92,22 @@ def show_search():
   # TODO can we use TagSearch.docid and Tag.rowid or something?
   # TODO can we match on a single column? maybe we need two tables?
 
-  #/search/?query=...
-  print(request.args)
-
   # TODO suggestion by Max: make sure that if someone searches for a tag in the search field, you return the tag
   # = get rid of the tag lookup field
   # TODO suggestion by Brian: implement different spellings of words, à la Google
 
-  query = "Eilenberg"
-  results = Tag.select(Tag, TagSearch, TagSearch.rank().alias("score")).join(TagSearch, on=(Tag.tag == TagSearch.tag).alias("search")).where(TagSearch.match(query), Tag.type.not_in(["chapter", "section", "subsection"]))
+  # return empty page (for now)
+  if "query" not in request.args:
+    return render_template("show_search.html", results=[])
+
+  # it might be a tag!
+  if len(request.args["query"]) == 4:
+    tag = Tag.select(Tag.tag).where(Tag.tag == request.args["query"])
+    if tag.count() == 1:
+      return redirect("tag/" + request.args["query"])
+
+  results = Tag.select(Tag, TagSearch, TagSearch.rank().alias("score")).join(TagSearch, on=(Tag.tag == TagSearch.tag).alias("search")).where(TagSearch.match(request.args["query"]), Tag.type.not_in(["chapter", "section", "subsection"]))
+  results = sorted(results)
   return render_template("show_search.html", results=results)
 
 @app.route("/tag/<string:tag>")
