c4d9f37dac84191f2a8f3cdde374197f6b284cc6
Pieter Belmans <pieterbelmans@gmail.com>
2018-02-07 14:00:27 +0100

    Small layout changes for history

--- gerby/static/js/history.js
+++ gerby/static/js/history.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("pre.commit-message").toggle();
   $("table#history td:first-child").css("cursor", "pointer");
-  //$("table#history td:first-child").prepend("<span style='float: right'>+</span>");
+
   $("table#history td:first-child").click(function() {
     $(this).find("pre.commit-message").toggle();
     $(this).toggleClass("clicked");
