9c5f1d6df32c46abf2320de6b122e59bde4611c5
Pieter Belmans <pieterbelmans@gmail.com>
2018-02-07 15:35:16 +0100

    Improved comment input toggle layout

--- gerby/static/js/toggle.js
+++ gerby/static/js/toggle.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   // toggle for comments
   $("section#post-comment h2 ~ *").toggle();
-  $("section#post-comment h2").addClass("comment-input-hidden");
+  $("section#post-comment > h2").css("cursor", "pointer");
+  $("section#post-comment h2").addClass("hidden");
 
   $("section#post-comment h2").on("click", function() {
-    $("section#post-comment h2").toggleClass("comment-input-hidden"); // can we do this in a one-liner?
-    $("section#post-comment h2").toggleClass("comment-input-visible");
+    $("section#post-comment h2").toggleClass("hidden"); // can we do this in a one-liner?
     $("section#post-comment h2 ~ *").toggle();
   });
 
