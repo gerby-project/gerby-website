1d30462d3fc81f131dedee7d7cd42aa471fb81ac
Pieter Belmans <pieterbelmans@gmail.com>
2018-02-08 12:03:33 +0100

    Previous / next bibliography entry

--- gerby/views/bibliography.py
+++ gerby/views/bibliography.py
@@ -3,15 +3,21 @@ from flask import render_template
 from gerby.gerby import app
 from gerby.database import *
 
-@app.route("/bibliography")
-def show_bibliography():
-  entries = BibliographyEntry.select()
-
+def decorateEntries(entries):
   for entry in entries:
     fields = BibliographyField.select().where(BibliographyField.key == entry.key)
+    # make the fields accessible in Jinja
     for field in fields:
       setattr(entry, field.field, field.value)
 
+  return entries
+
+
+
+@app.route("/bibliography")
+def show_bibliography():
+  entries = BibliographyEntry.select()
+  entries = decorateEntries(entries)
   entries = sorted(entries)
 
   return render_template("bibliography.overview.html", entries=entries)
@@ -25,6 +31,25 @@ def show_entry(key):
   for field in fields:
     entry.fields[field.field] = field.value
 
+  # it's too unpleasant to sort in SQL so we do it here, but the result might be a bit slow
+  entries = BibliographyEntry.select()
+  entries = decorateEntries(entries) # we need the author field to be present for sorting to work...
+  entries = sorted(entries)
+
+  index = -1
+  for i in range(len(entries)):
+    if entries[i].key == entry.key:
+      index = i
+
+  neighbours = [None, None]
+  if index > 0:
+    neighbours[0] = entries[index-1]
+  if index < len(entries) - 1:
+    neighbours[1] = entries[index+1]
+
   citations = Citation.select().join(Tag).where(Citation.key == entry.key).order_by(Tag.ref)
 
-  return render_template("bibliography.entry.html", entry=entry, citations=citations)
+  return render_template("bibliography.entry.html",
+                         entry=entry,
+                         neighbours=neighbours,
+                         citations=citations)
