e046f86158663569bf02c48ac1601cd012bffe15
Pieter Belmans <pieterbelmans@gmail.com>
2018-02-08 16:16:51 +0100

    More systematic use of macros (step 1)

--- gerby/static/js/toggle.js
+++ gerby/static/js/toggle.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   // we do this in JS as it only makes sense to have these when JS is enabled
-  $("div#burger-content").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='burger-toggle-numbering' value='burger-toggle-numbers' id='burger-toggle-numbers' checked><label for='burger-toggle-numbers'>show numbers</label></span>")
-  $("div#burger-content").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='burger-toggle-numbering' value='burger-toggle-tags' id='burger-toggle-tags'><label for='burger-toggle-tags'>show tags</label></span>")
+  $("div#burger-content div.interaction").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='burger-toggle-numbering' value='burger-toggle-numbers' id='burger-toggle-numbers' checked><label for='burger-toggle-numbers'>show numbers</label></span>")
+  $("div#burger-content div.interaction").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='burger-toggle-numbering' value='burger-toggle-tags' id='burger-toggle-tags'><label for='burger-toggle-tags'>show tags</label></span>")
 
-  $("div#interaction").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='meta-toggle-numbering' value='meta-toggle-numbers' id='meta-toggle-numbers' checked><label for='meta-toggle-numbers'>show numbers</label></span>")
-  $("div#interaction").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='meta-toggle-numbering' value='meta-toggle-tags' id='meta-toggle-tags'><label for='meta-toggle-tags'>show tags</label></span>")
+  $("section#meta div.interaction").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='meta-toggle-numbering' value='meta-toggle-numbers' id='meta-toggle-numbers' checked><label for='meta-toggle-numbers'>show numbers</label></span>")
+  $("section#meta div.interaction").append("<span class='toggle'><input type='radio' class='toggle-numbering' name='meta-toggle-numbering' value='meta-toggle-tags' id='meta-toggle-tags'><label for='meta-toggle-tags'>show tags</label></span>")
 
   $("input[type=radio][class=toggle-numbering]").on("change", function() {
     // the actual toggle
